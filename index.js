e2a57fc9c1ac0e4da96070420c0c1516f4be7df0
maryam saleh <87482765+maryam151@users.noreply.github.com>
2021-09-20 19:07:12 +0500

    Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+require('./startup/routes')(app);
+const db = require('./models/index');
+
+async function dbConnnect() {
+    await db.sequelize.sync();
+};
+
+dbConnnect();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+module.exports = server;
