1d28ebbd5ec1e54e0d73c7c0fd1903baca0541b7
maryam saleh <87482765+maryam151@users.noreply.github.com>
2021-09-21 00:43:40 +0500

    Conflicts Resloved

--- startup/config.js
+++ startup/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const config = require('config');
 
 // module.exports = function () {
@@ -10,16 +9,3 @@
 //         throw new Error('FATAL ERROR: jwtRefreshKey is not defined.');
 //     }
 // }
-=======
-const config = require('config');
-
-module.exports = function () {
-    if (!config.get('jwtPrivateKey')) {
-        throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
-    }
-
-    if (!config.get('jwtRefreshKey')) {
-        throw new Error('FATAL ERROR: jwtRefreshKey is not defined.');
-    }
-}
->>>>>>> cf784656b39749252186ec27165ffd49c76a2057
